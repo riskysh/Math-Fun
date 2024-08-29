65b318781d104519060481832f0e7e10626ae3e9
Risky <notriskysh@gmail.com>
2024-08-29 18:24:16 +0530

    First part of home page completed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,6 +14,12 @@ import Header from './Header.svelte'
 	<h1 class="text-white text-center text-7xl font-extrabold mt-28">Tell your story <br>with Illustrations</h1>
 	<p class="text-gray-400 text-center text-1xl font-semibold mt-3">Easily share your stunning Illustrations and Digital Art with artists across the globe.<br>
 		 Try it! it's kinda fun</p>
+	<div class="mt-4 flex w-full items-center justify-center gap-4">
+	<button class="text-white bg-[#57F287] px-4 p-3 rounded-lg hover:bg-[#30c45f] transition-all">Get Started</button>
+	<button class="text-white border border-[#27272a] px-4 p-3 rounded-lg hover:bg-[#27272a] transition-all">Learn more</button>
+	</div>
+
+	
 </section>
 
 <style>
