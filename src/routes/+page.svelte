de0f96080fdfa1c04d6e33eaf57941d8a40ce426
Risky <notriskysh@gmail.com>
2024-09-03 11:53:57 +0530

    Random number generator created

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,11 @@
 <script>
 
-	import { onMount, onDestroy } from "svelte";
-	let timer;
-	let seconds = 0
-	let isRunning = false
-
-	function startTimer() {
-		if (!isRunning) {
-			timer = setInterval(() => {
-				seconds++
-			}, 1000)
-			isRunning = true
-		}
+	function randomSingleDigit() {
+		return Math.floor(Math.random() * 9) + 1
 	}
 
-	function stopTimer() {
-		if (isRunning) {
-			clearInterval(timer)
-			isRunning = false
-		}
-	}
-
-	onMount(() => {
-		startTimer()
-	})
-
-	onDestroy(() => {
-		stopTimer()
-	})
-
-
-
-
+	const firstNum = randomSingleDigit()
+	const secondNum = randomSingleDigit()
 
 </script>
 
@@ -42,11 +16,11 @@
 
 <main>
 
-	<h2 class="text-xl mt-4 text-white text-center">{seconds} Seconds</h2>
-	<div class="mx-auto mt-80 justify-center flex grid-cols-2 gap-2">
-	<button class="px-4 py-2 bg-white rounded-lg" on:click={startTimer} disabled={isRunning}>Start</button>
-	<button class="px-4 py-2 bg-white rounded-lg" on:click={stopTimer} disabled={!isRunning}>Stop</button>
+	<div class="h-80 w-72 flex mx-auto my-40">
+		<h1 class="text-white text-9xl ml-36 h-2 w-2">{firstNum}</h1>
+		<h1 class="text-white text-9xl mt-32 mr-24 h-2 w-2">{secondNum}</h1>
 	</div>
+
 </main>
 
 <style>
